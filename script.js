a9cc37976c41164bd3862ba8b77e8bf112b38821
faizanwahla <faizanwahla470@gmail.com>
2022-09-25 22:32:31 +0500

    Bugs fixes + add score reset button

--- script.js
+++ script.js
@@ -1,21 +1,114 @@
 'use strict';
+// turn variable, scores
 let x = 0;
-let scoreOfX = 0;
-let scoreOfY = 0;
+let scoreOfX;
+let scoreOfY;
 
+// game over
+const gameOver = function () {
+  document
+    .querySelector('.grid-item-one')
+    .removeEventListener('click', clickEvent1);
+  document
+    .querySelector('.grid-item-two')
+    .removeEventListener('click', clickEvent2);
+  document
+    .querySelector('.grid-item-three')
+    .removeEventListener('click', clickEvent3);
+  document
+    .querySelector('.grid-item-four')
+    .removeEventListener('click', clickEvent4);
+  document
+    .querySelector('.grid-item-five')
+    .removeEventListener('click', clickEvent5);
+  document
+    .querySelector('.grid-item-six')
+    .removeEventListener('click', clickEvent6);
+  document
+    .querySelector('.grid-item-seven')
+    .removeEventListener('click', clickEvent7);
+  document
+    .querySelector('.grid-item-eight')
+    .removeEventListener('click', clickEvent8);
+  document
+    .querySelector('.grid-item-nine')
+    .removeEventListener('click', clickEvent9);
+};
+const addEvent = function () {
+  document
+    .querySelector('.grid-item-one')
+    .addEventListener('click', clickEvent1);
+  document
+    .querySelector('.grid-item-two')
+    .addEventListener('click', clickEvent2);
+  document
+    .querySelector('.grid-item-three')
+    .addEventListener('click', clickEvent3);
+  document
+    .querySelector('.grid-item-four')
+    .addEventListener('click', clickEvent4);
+  document
+    .querySelector('.grid-item-five')
+    .addEventListener('click', clickEvent5);
+  document
+    .querySelector('.grid-item-six')
+    .addEventListener('click', clickEvent6);
+  document
+    .querySelector('.grid-item-seven')
+    .addEventListener('click', clickEvent7);
+  document
+    .querySelector('.grid-item-eight')
+    .addEventListener('click', clickEvent8);
+  document
+    .querySelector('.grid-item-nine')
+    .addEventListener('click', clickEvent9);
+};
+
+// score reset
+
+const scoreReseter = function () {
+  localStorage.setItem('scoreOfX', 0);
+  localStorage.setItem('scoreOfY', 0);
+  scoreOfX = Number(localStorage.getItem('scoreOfX'));
+  scoreOfY = Number(localStorage.getItem('scoreOfY'));
+  document.querySelector('.scoreOfX').textContent = scoreOfX;
+  document.querySelector('.scoreOfY').textContent = scoreOfY;
+};
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.resetScore').addEventListener('click', scoreReseter);
+});
+
+// score fetcher
+document.addEventListener('DOMContentLoaded', function () {
+  if (!localStorage.getItem('scoreOfX') && !localStorage.getItem('scoreOfY')) {
+    localStorage.setItem('scoreOfX', 0);
+    localStorage.setItem('scoreOfY', 0);
+  }
+  scoreOfX = Number(localStorage.getItem('scoreOfX'));
+  scoreOfY = Number(localStorage.getItem('scoreOfY'));
+  document.querySelector('.scoreOfX').textContent = scoreOfX;
+  document.querySelector('.scoreOfY').textContent = scoreOfY;
+});
 // score updater
 const scoreUpdaterX = function () {
+  gameOver();
   scoreOfX++;
-  document.querySelector('.scoreOfX').textContent = scoreOfX;
+  localStorage.setItem('scoreOfX', scoreOfX);
+  document.querySelector('.scoreOfX').textContent =
+    localStorage.getItem('scoreOfX');
 };
 const scoreUpdaterY = function () {
+  gameOver();
   scoreOfY++;
-  document.querySelector('.scoreOfY').textContent = scoreOfY;
+  localStorage.setItem('scoreOfY', scoreOfY);
+  document.querySelector('.scoreOfY').textContent =
+    localStorage.getItem('scoreOfY');
 };
 // Play Again function
 const playAgain = function () {
   x = 0;
   disablePlayAgainBtn();
+  addEvent();
   document.querySelector('h2').style.color = '#ffffff';
   document.querySelector('h2').textContent = 'null';
   document.querySelector('.grid-item-one').textContent = '1';
@@ -48,6 +141,7 @@ const enablePlayAgainBtn = function () {
 // winner functions
 
 const winner5 = function () {
+  enablePlayAgainBtn();
   document.querySelector('h2').style.color = '#000000';
   document.querySelector('h2').textContent = `'${document
     .querySelector('.grid-item-five')
@@ -58,9 +152,9 @@ const winner5 = function () {
   if (document.querySelector('.grid-item-five').textContent === 'y') {
     scoreUpdaterY();
   }
-  enablePlayAgainBtn();
 };
 const winner2 = function () {
+  enablePlayAgainBtn();
   document.querySelector('h2').style.color = '#000000';
   document.querySelector('h2').textContent = `'${document
     .querySelector('.grid-item-two')
@@ -71,9 +165,9 @@ const winner2 = function () {
   if (document.querySelector('.grid-item-two').textContent === 'y') {
     scoreUpdaterY();
   }
-  enablePlayAgainBtn();
 };
 const winner4 = function () {
+  enablePlayAgainBtn();
   document.querySelector('h2').style.color = '#000000';
   document.querySelector('h2').textContent = `'${document
     .querySelector('.grid-item-four')
@@ -84,9 +178,9 @@ const winner4 = function () {
   if (document.querySelector('.grid-item-four').textContent === 'y') {
     scoreUpdaterY();
   }
-  enablePlayAgainBtn();
 };
 const winner6 = function () {
+  enablePlayAgainBtn();
   document.querySelector('h2').style.color = '#000000';
   document.querySelector('h2').textContent = `'${document
     .querySelector('.grid-item-six')
@@ -97,9 +191,9 @@ const winner6 = function () {
   if (document.querySelector('.grid-item-six').textContent === 'y') {
     scoreUpdaterY();
   }
-  enablePlayAgainBtn();
 };
 const winner8 = function () {
+  enablePlayAgainBtn();
   document.querySelector('h2').style.color = '#000000';
   document.querySelector('h2').textContent = `'${document
     .querySelector('.grid-item-eight')
@@ -110,31 +204,17 @@ const winner8 = function () {
   if (document.querySelector('.grid-item-eight').textContent === 'y') {
     scoreUpdaterY();
   }
-  enablePlayAgainBtn();
 };
 const draw = function () {
+  enablePlayAgainBtn();
   document.querySelector('h2').style.color = '#000000';
   document.querySelector('h2').textContent = 'Draw';
-  enablePlayAgainBtn();
 };
 
 // win/lose detections function
 
 const ai = function () {
   if (
-    x >= 9 &&
-    document.querySelector('.grid-item-one').textContent != '1' &&
-    document.querySelector('.grid-item-two').textContent != '2' &&
-    document.querySelector('.grid-item-three').textContent != '3' &&
-    document.querySelector('.grid-item-four').textContent != '4' &&
-    document.querySelector('.grid-item-five').textContent != '5' &&
-    document.querySelector('.grid-item-six').textContent != '6' &&
-    document.querySelector('.grid-item-seven').textContent != '7' &&
-    document.querySelector('.grid-item-eight').textContent != '8' &&
-    document.querySelector('.grid-item-nine').textContent != '9'
-  ) {
-    draw();
-  } else if (
     document.querySelector('.grid-item-one').textContent ===
       document.querySelector('.grid-item-two').textContent &&
     document.querySelector('.grid-item-two').textContent ===
@@ -160,6 +240,7 @@ const ai = function () {
         document.querySelector('.grid-item-eight').textContent)
   ) {
     winner5();
+    gameOver();
   } else if (
     document.querySelector('.grid-item-seven').textContent ===
       document.querySelector('.grid-item-eight').textContent &&
@@ -167,6 +248,7 @@ const ai = function () {
       document.querySelector('.grid-item-nine').textContent
   ) {
     winner8();
+    gameOver();
   } else if (
     document.querySelector('.grid-item-one').textContent ===
       document.querySelector('.grid-item-four').textContent &&
@@ -174,6 +256,7 @@ const ai = function () {
       document.querySelector('.grid-item-seven').textContent
   ) {
     winner4();
+    gameOver();
   } else if (
     document.querySelector('.grid-item-three').textContent ===
       document.querySelector('.grid-item-six').textContent &&
@@ -181,11 +264,137 @@ const ai = function () {
       document.querySelector('.grid-item-nine').textContent
   ) {
     winner6();
+    gameOver();
+  } else if (
+    x >= 9 &&
+    document.querySelector('.grid-item-one').textContent != '1' &&
+    document.querySelector('.grid-item-two').textContent != '2' &&
+    document.querySelector('.grid-item-three').textContent != '3' &&
+    document.querySelector('.grid-item-four').textContent != '4' &&
+    document.querySelector('.grid-item-five').textContent != '5' &&
+    document.querySelector('.grid-item-six').textContent != '6' &&
+    document.querySelector('.grid-item-seven').textContent != '7' &&
+    document.querySelector('.grid-item-eight').textContent != '8' &&
+    document.querySelector('.grid-item-nine').textContent != '9'
+  ) {
+    draw();
+    gameOver();
   }
 };
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.playAgain').addEventListener('click', playAgain);
 });
+
+// game turn functions
+
+const clickEvent1 = function () {
+  if (x < 9 && document.querySelector('.grid-item-one').textContent === '1') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-one').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-one').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-one').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent2 = function () {
+  if (x < 9 && document.querySelector('.grid-item-two').textContent === '2') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-two').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-two').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-two').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent3 = function () {
+  if (x < 9 && document.querySelector('.grid-item-three').textContent === '3') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-three').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-three').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-three').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent4 = function () {
+  if (x < 9 && document.querySelector('.grid-item-four').textContent === '4') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-four').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-four').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-four').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent5 = function () {
+  if (x < 9 && document.querySelector('.grid-item-five').textContent === '5') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-five').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-five').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-five').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent6 = function () {
+  if (x < 9 && document.querySelector('.grid-item-six').textContent === '6') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-six').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-six').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-six').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent7 = function () {
+  if (x < 9 && document.querySelector('.grid-item-seven').textContent === '7') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-seven').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-seven').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-seven').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent8 = function () {
+  if (x < 9 && document.querySelector('.grid-item-eight').textContent === '8') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-eight').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-eight').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-eight').style.color = '#000000';
+  x++;
+  ai();
+};
+const clickEvent9 = function () {
+  if (x < 9 && document.querySelector('.grid-item-nine').textContent === '9') {
+    if (x === 0 || x % 2 == 0) {
+      document.querySelector('.grid-item-nine').textContent = 'x';
+    } else {
+      document.querySelector('.grid-item-nine').textContent = 'y';
+    }
+  }
+  document.querySelector('.grid-item-nine').style.color = '#000000';
+  x++;
+  ai();
+};
 
 // Game turn logic
 
@@ -194,125 +403,46 @@ document.addEventListener('DOMContentLoaded', function () {
     //one
     document
       .querySelector('.grid-item-one')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-one').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-one').textContent = 'y';
-        }
-        document.querySelector('.grid-item-one').style.color = '#000000';
-        x++;
-      });
+      .addEventListener('click', clickEvent1);
 
     //two
     document
       .querySelector('.grid-item-two')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-two').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-two').textContent = 'y';
-        }
-        document.querySelector('.grid-item-two').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent2);
 
     // three
     document
       .querySelector('.grid-item-three')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-three').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-three').textContent = 'y';
-        }
-        document.querySelector('.grid-item-three').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent3);
 
     // four
     document
       .querySelector('.grid-item-four')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-four').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-four').textContent = 'y';
-        }
-        document.querySelector('.grid-item-four').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent4);
 
     //five
     document
       .querySelector('.grid-item-five')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-five').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-five').textContent = 'y';
-        }
-        document.querySelector('.grid-item-five').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent5);
 
     //six
     document
       .querySelector('.grid-item-six')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-six').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-six').textContent = 'y';
-        }
-        document.querySelector('.grid-item-six').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent6);
+
     //seven
     document
       .querySelector('.grid-item-seven')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-seven').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-seven').textContent = 'y';
-        }
-        document.querySelector('.grid-item-seven').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent7);
 
     //eight
     document
       .querySelector('.grid-item-eight')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-eight').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-eight').textContent = 'y';
-        }
-        document.querySelector('.grid-item-eight').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent8);
 
     //nine
     document
       .querySelector('.grid-item-nine')
-      .addEventListener('click', function () {
-        if (x === 0 || x % 2 == 0) {
-          document.querySelector('.grid-item-nine').textContent = 'x';
-        } else {
-          document.querySelector('.grid-item-nine').textContent = 'y';
-        }
-        document.querySelector('.grid-item-nine').style.color = '#000000';
-        x++;
-        ai();
-      });
+      .addEventListener('click', clickEvent9);
   }
 });
